b7aa8b82f74626e173884c3e0d642c30e2a020cf
Pshypher <bshote@getcarbon.co>
2024-12-14 12:29:19 +0100

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,7 @@
+mod section_01;
+
+fn main() {
+    use section_01::program_09::demo;
+
+    demo::main()
+}
---
+++ src/section_01/program_09.rs
@@ -0,0 +1,42 @@
+#[derive(Debug)]
+pub struct LinkedList<T> {
+    data: T,
+    next: Option<Box<LinkedList<T>>>,
+}
+
+impl <T: std::ops::AddAssign> LinkedList<T> {
+    pub fn add_up(&mut self, value: T) {
+        self.data += value;
+    }
+}
+
+pub mod demo {
+
+    use super::LinkedList;
+
+    pub fn main() {
+         let mut ll = LinkedList {
+             data: 3,
+             next: Some(Box::new(LinkedList {
+                 data: 2,
+                 next: None,
+             })),
+         };
+
+        if let Some(ref mut v) = ll.next {
+            v.add_up(10);
+        }
+
+        let mut v= Vec::with_capacity(100);
+        v.push("hello".to_string());
+        v.push("goodbye".to_string());
+
+        for i in 0..105 {
+            v.push(i.to_string());
+        }
+
+        println!("v.len = {}, capacity = {}", v.len(), v.capacity());
+
+        println!("Hello, {:?}", ll);
+     }
+}
